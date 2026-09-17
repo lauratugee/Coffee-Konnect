b643f9fcc0ed9086ff6f657545556db33d72462a
lauratugee <Laura.Tugee@strathmore.edu>
2026-09-17 22:48:42 +0300

    changes

--- app/src/main/java/com/example/coffeekonnect/MainActivity.kt
+++ app/src/main/java/com/example/coffeekonnect/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.coffeekonnect.ui.theme.CoffeeKonnectTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +30,79 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CoffeeKonnectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                CoffeeKonnectWelcomePage()
+
                 }
             }
         }
-    }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CoffeeKonnectTheme {
-        Greeting("Android")
+fun CoffeeKonnectWelcomePage(){
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ){ innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .safeDrawingPadding()
+                .padding(
+                    start = 30.dp,
+                    end = 30.dp,
+                    top =20.dp,
+                    bottom =20.dp
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+          Text(
+              text= "COFFEE KONNECT",
+              fontSize = 32.sp,
+              fontWeight = FontWeight.Bold,
+              textAlign = TextAlign.Center
+          )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text= "Linking smallholder coffee farmers across Kenya",
+                fontSize = 17.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier=Modifier.height(50.dp))
+
+            Button(
+                onClick = {
+                    //Login page
+                },
+                modifier= Modifier.fillMaxSize()
+            ){
+            Text(
+                text = "Login",
+                fontSize = 16.sp
+
+            )
+        }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedButton(
+                onClick = {
+                    //Registration screen
+                },
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text="Register",
+                    fontSize = 16.sp
+                )
+            }
+
+
+        }
+        }
+
     }
-}
+
+
+
+
