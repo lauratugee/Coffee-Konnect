12b63bf5aebb66ec534611e7dfb6823049a1cab1
lauratugee <Laura.Tugee@strathmore.edu>
2026-09-17 22:57:18 +0300

    changes

--- app/src/main/java/com/example/coffeekonnect/MainActivity.kt
+++ app/src/main/java/com/example/coffeekonnect/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeDrawingPadding
@@ -75,7 +76,7 @@ fun CoffeeKonnectWelcomePage(){
                 onClick = {
                     //Login page
                 },
-                modifier= Modifier.fillMaxSize()
+                modifier= Modifier.fillMaxWidth()
             ){
             Text(
                 text = "Login",
@@ -89,7 +90,7 @@ fun CoffeeKonnectWelcomePage(){
                 onClick = {
                     //Registration screen
                 },
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
                     text="Register",
